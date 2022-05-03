9bcbec342ec9a2f5e41118e2b8c65f6e2533a6ea
HumbertoBPF <humbertobpf@gmail.com>
2022-05-03 16:39:29 -0300

    Using redirect after post in the vocabulary game

--- languageschool/views.py
+++ languageschool/views.py
@@ -1,5 +1,7 @@
+from urllib.parse import urlencode
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
+from django.urls import reverse
 from languageschool.models import Language, Word
 import random
 
@@ -36,7 +38,7 @@ def vocabulary_game(request):
             else:
                 # Verifying if the selected languages are equal
                 if base_language != target_language:
-                    # Validating the base_language (check if it is a valid language)
+                    # Validating the base_language (check if it is a valid language, if not it returns a 404 error)
                     base_language = get_object_or_404(Language, language_name = base_language)
                     # Picking all the words corresponding to the target language
                     words_list = Word.objects.filter(language = get_object_or_404(Language, language_name = target_language))
@@ -44,7 +46,7 @@ def vocabulary_game(request):
                     return render(request, "vocabulary_game.html", {"word":selected_word, "base_language":base_language})
                 else:
                     messages.error(request, "The target and base languages must be different")
-    # If some error was detect, go to the setup page
+    # If some error was detected, go to the setup page
     return vocabulary_game_setup(request)
 
 def vocabulary_game_verify_answer(request):
@@ -54,19 +56,30 @@ def vocabulary_game_verify_answer(request):
             translation_word = request.POST["translation_word"]
             base_language = get_object_or_404(Language, language_name = request.POST["base_language"])
             word_to_translate = get_object_or_404(Word, pk = word_to_translate_id)
+            
+            correct_translation = ""
             is_translation_correct = False
             for synonim in word_to_translate.synonym.all():
-                if synonim.language == base_language and str(synonim) == translation_word:
-                    is_translation_correct = True
-                    break
+                if synonim.language == base_language:
+                    # Getting the correct answer
+                    if len(correct_translation) != 0:
+                        correct_translation += ", "
+                    correct_translation += str(synonim)
+                    # Checking if the answer was correct (if the user provided a synonim and if the synonim is in the correct language)
+                    if str(synonim) == translation_word:
+                        is_translation_correct = True
+            # Message of feedback for the user
             if is_translation_correct:
-                messages.success(request, "Correct :)")
+                messages.success(request, "Correct :)\n"+str(word_to_translate) + ": "+str(correct_translation))
             else:
-                messages.error(request, "Wrong answer, don't give up and ask help for Google Translator if necessary")
-            return render(request, "vocabulary_game.html", 
-                {"word": word_to_translate, 
-                "base_language": base_language, 
-                "last_answer": translation_word ,
-                "is_translation_correct": is_translation_correct})
+                messages.error(request, "Wrong answer\n"+str(word_to_translate) + ": "+str(correct_translation))
+            base_url = reverse('vocabulary_game')
+            query_string =  urlencode({'base_language': str(base_language), 
+                                'target_language': str(word_to_translate.language)})
+            url = '{}?{}'.format(base_url, query_string)
+            return redirect(url)
     # If some error was detect, go to the setup page
     return vocabulary_game_setup(request)
+
+def article_game_setup(request):
+    return render(request, 'article_game_setup.html')
