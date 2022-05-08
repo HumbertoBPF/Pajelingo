f48664aa64bd0b8abd3f256eff196f4467263094
HumbertoBPF <humbertobpf@gmail.com>
2022-05-07 23:47:27 -0300

    Creating search tool

--- languageschool/views/general.py
+++ languageschool/views/general.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from languageschool.models import Word
 
 
 # Create your views here.
@@ -15,3 +17,18 @@ def request_contains(request_with_method, variables_required):
 
 def index(request):
     return render(request, 'index.html')
+
+def search(request):
+    if request.method == "GET":
+        if request_contains(request.GET, ["search"]):
+            search = request.GET["search"]
+            # Results containing the specified string
+            search_results = Word.objects.filter(word_name__icontains = search)
+            # Pagination(getting page number and the results of the current page)
+            paginator = Paginator(search_results, 12)
+            page = request.GET.get('page')
+            search_results_current_page = paginator.get_page(page)
+
+            return render(request, 'search.html', {'search_results': search_results_current_page, 'search': search})
+    
+    return index(request)
