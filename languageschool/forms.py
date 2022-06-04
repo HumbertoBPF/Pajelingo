3494e78be436dd3333afe4e9ad1ad2bad37ea51c
HumbertoBPF <humbertobpf@gmail.com>
2022-06-04 17:21:09 -0300

    :fire:[hotfix] removing some files from repo:fire:

--- languageschool/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class FormPicture(forms.Form):
-    '''Form to update profile picture'''
-    picture = forms.ImageField(label='Change profile picture', required=False)
--- languageschool/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import article_game
-from . import conjugation_game
-from . import general
-from . import vocabulary_game
-from . import account
