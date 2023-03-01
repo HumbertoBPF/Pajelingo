592b3cea1b5f49a5ce3e6acf478737419610505d
HumbertoBPF <humbertobpf@gmail.com>
2023-03-01 02:12:54 -0300

    :sparkles: Implementing API to get specific words and meanings :sparkles:

---
+++ create_user_and_scores.py
@@ -0,0 +1,16 @@
+import random
+
+from django.contrib.auth.models import User
+from django.utils.crypto import get_random_string
+
+from languageschool.models import Game, Language, Score
+
+numberOfUsers = 20
+
+for i in range(numberOfUsers):
+    user = User.objects.create_user(username=get_random_string(100),
+                                    email=get_random_string(20) + "@test.com",
+                                    password="str0ng-p4ssw0rd")
+    for language in Language.objects.all():
+        for game in Game.objects.all():
+            Score.objects.create(user=user, language=language, game=game, score=random.randint(10, 1000))
--- languageschool/views/api.py
+++ languageschool/views/api.py
@@ -1,9 +1,13 @@
-from django.db.models.functions import Lower
-from rest_framework import views, generics
+import base64
 
-from languageschool.models import Language, Word
+from django.db.models.functions import Lower
+from django.shortcuts import get_object_or_404
+from rest_framework import views, generics, status
+from rest_framework.response import Response
+
+from languageschool.models import Language, Word, Meaning
 from languageschool.paginators import SearchPaginator
-from languageschool.serializers import WordSerializer
+from languageschool.serializers import WordSerializer, MeaningSerializer
 
 
 class SearchView(generics.ListAPIView):
@@ -20,3 +24,23 @@ class SearchView(generics.ListAPIView):
         return Word.objects \
             .filter(word_name__icontains=search_pattern, language__in=languages).order_by(Lower('word_name'))
 
+
+class MeaningView(views.APIView):
+    def get(self, request, pk):
+        word = get_object_or_404(Word, pk=pk)
+        meanings = Meaning.objects.filter(word=word)
+        serializer = MeaningSerializer(meanings, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class WordView(views.APIView):
+    def get(self, request, pk):
+        word = get_object_or_404(Word, pk=pk)
+        serializer = WordSerializer(word)
+
+        word_image = None
+        if word.image:
+            img = word.image.open("rb")
+            word_image = base64.b64encode(img.read())
+
+        return Response(data={**serializer.data, "image": word_image}, status=status.HTTP_200_OK)
