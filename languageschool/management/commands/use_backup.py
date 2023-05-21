2cd5dea9162bc3b52232ef792cbeac84c8cc4c00
HumbertoBPF <humbertobpf@gmail.com>
2023-05-21 15:57:11 -0300

    :card_file_box: Filling tables that have no foreign keys from backup files :card_file_box:

--- languageschool/management/commands/use_backup.py
+++ languageschool/management/commands/use_backup.py
@@ -5,36 +5,25 @@ from django.core.management import BaseCommand
 
 from languageschool.models import Game, Category, Article, Word, Meaning, Conjugation, Score, GameRound, Language, User
 
-
-def replace_none_strings(row):
-    columns = []
-    for column in row:
-        columns.append(None if column == "None" else column)
-    return columns
-
-def get_user_table():
-    with open(f'C:/Users/Humberto/Desktop/Humberto/Study/WebDev/Pajelingo/backups/User.csv', mode='r') as f:
+def fill_table(cls):
+    with open(f'C:/Users/Humberto/Desktop/Humberto/Study/WebDev/Pajelingo/backups/{cls.__name__}.csv', mode='r') as f:
         rows = csv.reader(f)
-        next(rows, None)
+        headers = next(rows, None)
 
         for row in rows:
-            columns = replace_none_strings(row)
+            args = {}
 
-            pk, username, email, is_active, is_staff, last_login, date_joined = columns
-            user = User.objects.create(
-                pk=pk,
-                username=username,
-                email=email,
-                is_active=is_active,
-                is_staff=is_staff,
-                last_login=last_login,
-                date_joined=date_joined
-            )
-            user.set_password("str0ng-p4ssw0rd")
-            user.save()
+            for i in range(len(row)):
+                header = headers[i]
+                column = row[i]
+                args[header] = None if column == "None" else column
 
+            cls.objects.create(**args)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        get_user_table()
+        # fill_user_table()
+        # fill_table(Game)
+        # fill_table(Language)
+        fill_table(Category)
